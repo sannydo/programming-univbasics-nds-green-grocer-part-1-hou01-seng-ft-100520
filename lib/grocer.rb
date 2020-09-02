ad8a2a1cc1ddf5c4570390b9982252d9f0404018
Sanny Do <sanny.do94@gmail.com>
2020-09-02 00:57:41 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,6 +27,7 @@ def consolidate_cart(cart)
     #take each item in the cart and see if it is already in the new cart
     if cart[:item] = new_cart
       #calling the find_item_by_name_in_collection to check if item is in the new cart. Second argument is the collection we want to search want to see if the item is in our new cart.
+      #So this will return either the item found or if not found it will return nil
       new_cart_item = find_item_by_name_in_collection(cart[:counter][:item], new_cart)
     #to ensure there is not an infinite loop 
      counter += 1
