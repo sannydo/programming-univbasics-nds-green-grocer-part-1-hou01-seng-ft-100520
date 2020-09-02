141a9046b84dda3c8bac6b95a98d66f61438e680
Sanny Do <sanny.do94@gmail.com>
2020-09-02 01:26:54 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -29,25 +29,24 @@ def consolidate_cart(cart)
  while counter < cart.length 
     
     #take each item in the cart and see if it is already in the new cart
-  #if cart[:item] = new_cart
-      
-      #calling the find_item_by_name_in_collection to check if item is in the new cart. Second argument is the collection we want to search want to see if the item is in our new cart.
-      #So this will return either the item found or if not found it will return nil
-    new_cart_item = find_item_by_name_in_collection(cart[:counter][:item], new_cart)
+    #calling the find_item_by_name_in_collection to check if item is in the new cart. Second argument is the collection we want to search want to see if the item is in our new cart.
+    #So this will return either the item found or if not found it will return nil
+    
+   new_cart_item = find_item_by_name_in_collection(cart[:counter][:item], new_cart)
       #if item is already in cart, we just want to increase the count
-      if new_cart_item != nil 
+    if new_cart_item != nil 
         
         #if new_cart_item does not equal nil then we want to access new_cart_item and inside we want to access the count and increase by 1
-        new_cart_item[:count] += 1
+      new_cart_item[:count] += 1
         
         #otherwise, item is nil. We would need to create our item. Inside the hash we need to construct the new item in the new cart
-      else
-        new_cart_item = {
-          :item => cart[:counter][:item], 
-          :price => cart[:counter][:price],
-          :clearance => cart[:counter][:clearance],
+    else
+      new_cart_item = {
+        :item => cart[:counter][:item], 
+        :price => cart[:counter][:price],
+        :clearance => cart[:counter][:clearance],
           #this is the first time count is being used, the old item did not have count
-          :count => 1
+        :count => 1
         }
         #after the new_cart is built, we are going to shovel new_cart_item variable
         new_cart << new_cart_item
